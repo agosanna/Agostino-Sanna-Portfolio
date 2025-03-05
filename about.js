80690868b6a8abd7bf090689ae59019f96d072ab
agosanna <agostino.sanna@mail.polimi.it>
2025-03-05 14:40:23 +0100

    about fix

--- about.js
+++ about.js
@@ -47,38 +47,83 @@ const imageSets = {
     "./public/mediaprojects/about/analog_4.jpg",
 ],
 
+"communication": [
+    "./public/mediaprojects/about/c_1.jpeg",
+    "./public/mediaprojects/about/c_2.jpg",
+    "./public/mediaprojects/about/c_3.jpg",
+],
 
+"did" : [
+    "./public/mediaprojects/about/did_1.jpg",
+    "./public/mediaprojects/about/did_2.jpg",
+    "./public/mediaprojects/about/did_3.jpg",
+],
 };
 
+let visibleImg;
+
 links.forEach(link => {
-    link.addEventListener("mouseenter", (event) => {
-        imageContainer.innerHTML = ""; // Clear previous images
-        
-        const linkId = event.target.id;
-        console.log(linkId);
-        const images = imageSets[linkId];
+    if (window.innerWidth > 768) {
+        link.addEventListener("mouseenter", (event) => {
+            imageContainer.innerHTML = ""; // Clear previous images
+            
+            const linkId = event.target.id;
+            console.log(linkId);
+            const images = imageSets[linkId];
 
-        // Position the image container below the link
-        const aboutRect = document.getElementById("about").getBoundingClientRect();
-        const centerY = (aboutRect.bottom - aboutRect.top) / 2 + window.scrollY;        
+            // Position the image container below the link
+            const aboutRect = document.getElementById("about").getBoundingClientRect();
+            const centerY = (aboutRect.bottom - aboutRect.top) / 2 + window.scrollY / 2;        
 
-        imageContainer.style.top = `${centerY}px`;
+            imageContainer.style.top = `${centerY}px`;
 
-        images.forEach((src) => {
-            let img = document.createElement("img");
-            img.src = src;
-            img.style.marginLeft = `${Math.random() * 6 - 3}vw`;
-            img.style.marginRight = `${Math.random() * 6 - 3}vw`;
-            img.style.marginTop = `${Math.random() * 20 - 10}vh`;
-            img.style.marginBottom = `${Math.random() * 20 - 10}vh`;
-            imageContainer.appendChild(img);
+            images.forEach((src) => {
+                let img = document.createElement("img");
+                img.src = src;
+                img.style.marginLeft = `${Math.random() * 6 - 3}vw`;
+                img.style.marginRight = `${Math.random() * 6 - 3}vw`;
+                img.style.marginTop = `${Math.random() * 20 - 10}vh`;
+                img.style.marginBottom = `${Math.random() * 20 - 10}vh`;
+                imageContainer.appendChild(img);
+            });
         });
 
-    });
+        link.addEventListener("mouseleave", () => {
+            setTimeout(() => {
+                imageContainer.innerHTML = "";
+            }, 200);
+        });
+    } else {
+        link.addEventListener("click", (event) => {
+            imageContainer.innerHTML = ""; // Clear previous images
+            visibleImg = true;
+            const linkId = event.target.id;
+            console.log(linkId);
+            const images = imageSets[linkId];
 
-    link.addEventListener("mouseleave", () => {
-        setTimeout(() => {
-            imageContainer.innerHTML = "";
-        }, 200);
-    });
+            // Position the image container below the link
+            const aboutRect = document.getElementById("about").getBoundingClientRect();
+            const centerY = (aboutRect.bottom - aboutRect.top) / 2 + window.scrollY;        
+
+            imageContainer.style.top = `${centerY}px`;
+
+            images.forEach((src) => {
+                let img = document.createElement("img");
+                img.src = src;
+                imageContainer.appendChild(img);
+                img.style.marginLeft = `${Math.random() * 20}vw`;
+                img.style.marginTop = `${Math.random() * 5 }vh`;
+                img.style.marginBottom = `${Math.random() * 5 }vh`;
+            });
+            if (visibleImg == true) {
+                document.addEventListener("click", (event) => {
+                    const aboutContainer = document.getElementById("about");
+                    if (!aboutContainer.contains(event.target) && !imageContainer.contains(event.target)) {
+                        imageContainer.innerHTML = "";
+                    }
+                    visibleImg = false;
+                });
+            }
+        });
+    }
 });
